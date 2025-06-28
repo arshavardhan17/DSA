7499e4446437f1d0db2dd0d951ffa45bf5919121
ArshaVardhan <2300031000@kluniversity.in>
2025-06-28 09:49:50 +0000

    Add binary conversion implementation and related images for number system fundamentals

---
+++ Section6_Fundamentals/NumberSystem_51/Main.java
@@ -0,0 +1,19 @@
+package Section6_Fundamentals.NumberSystem_51;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Sc= new Scanner(System.in);
+        int a=Sc.nextInt();
+        String result =" ";
+        while (a>0) {
+            int remainder=a%2;
+            a=a/2;
+            result =remainder +result; //reverse the answer
+        }
+        System.out.println(result);
+        Sc.close();
+    }
+    
+}
