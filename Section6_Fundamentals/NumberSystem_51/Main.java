bdd6795f9612ffa1751a9a96616943bfbed00ad8
ArshaVardhan <2300031000@kluniversity.in>
2025-06-30 20:41:16 +0000

    Refactor formatting and spacing in Main.java for improved readability

--- Section6_Fundamentals/NumberSystem_51/Main.java
+++ Section6_Fundamentals/NumberSystem_51/Main.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner Sc= new Scanner(System.in);
-        int a=Sc.nextInt();
-        String result =" ";
-        while (a>0) {
-            int remainder=a%2;
-            a=a/2;
-            result =remainder +result; //reverse the answer
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        String result = " ";
+        while (a > 0) {
+            int remainder = a % 2;
+            a = a / 2;
+            result = remainder + result; // reverse the answer
         }
         System.out.println(result);
         Sc.close();
     }
-    
+
 }
