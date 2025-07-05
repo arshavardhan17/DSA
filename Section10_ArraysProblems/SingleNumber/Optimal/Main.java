57d994f36767f3e689a200babad60dde9933a1ab
ArshaVardhan <2300031000@kluniversity.in>
2025-07-05 23:26:50 +0000

    Restore original functionality in Main class for SingleNumber problem

--- Section10_ArraysProblems/SingleNumber/Optimal/Main.java
+++ Section10_ArraysProblems/SingleNumber/Optimal/Main.java
@@ -4,21 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner Sc = new Scanner(System.in);
-        // int n = Sc.nextInt();
-        // int[] arr = new int[n];
-        // for (int i = 0; i < n; i++) {
-        // arr[i] = Sc.nextInt();
-        // }
-        // Sc.close();
-        // int ans = 0;
-        // for (int num : arr) {
-        // ans = ans ^ num; // ^ retuns 0 if both the numbers are same 2^2=0
-        // }
-        // System.out.println(ans);
-        String s = "vssd+eafnsf";
-        String[] str = s.split("\\+");
-        System.out.println(str[1]);
+        Scanner Sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Sc.nextInt();
+        }
+        Sc.close();
+        int ans = 0;
+        for (int num : arr) {
+            ans = ans ^ num; // ^ retuns 0 if both the numbers are same 2^2=0
+        }
+        System.out.println(ans);
+
     }
 
 }
