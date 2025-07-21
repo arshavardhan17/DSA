5de614710d123db9657d9fbc8c8bd0eca76dee38
ArshaVardhan <2300031000@kluniversity.in>
2025-07-21 12:42:36 +0000

    Format code for consistency and improve readability in Main.java

--- Section6_Fundamentals/Decimal_To_Binary_55/Main.java
+++ Section6_Fundamentals/Decimal_To_Binary_55/Main.java
@@ -1,17 +1,18 @@
 package Section6_Fundamentals.Decimal_To_Binary_55;
 
 import java.util.Scanner;
+
 //Task:change the  dcimal number to binary using bitwise
 public class Main {
     public static void main(String[] args) {
-        Scanner Sc =new Scanner(System.in);
-        int num =Sc.nextInt();
-        
-        String count ="";   //int count =0;
-        while(num>0){  
-           int lastBit=num&1;
-            count =count+lastBit;  //count=count*10+lastbit
-            num=num>>1;
+        Scanner Sc = new Scanner(System.in);
+        int num = Sc.nextInt();
+
+        String count = ""; // int count =0;
+        while (num > 0) {
+            int lastBit = num & 1;
+            count = count + lastBit; // count=count*10+lastbit
+            num = num >> 1;
         }
         System.out.println(count);
         Sc.close();
